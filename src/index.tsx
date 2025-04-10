793aca3b5933b8c518712bcfe08b1650ee953b50
KazancevE <kazanceve.gor@gmail.com>
2025-04-11 00:03:48 +0700

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 import { ConfigProvider } from 'antd';
 import ru from 'antd/locale/ru_RU'
+import { AuthProvider } from './share/AuthProvider';
 const container = document.getElementById('root');
 if (!container) throw new Error('Failed to find the root element');
 
@@ -11,8 +12,10 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    <AuthProvider>
     <ConfigProvider locale={ru}>
       <App />
     </ConfigProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
