1edb5129de72e7d9aa2850f3cc221128bce18252
KazancevE <kazanceve.gor@gmail.com>
2025-04-11 01:17:53 +0700

    fix

--- src/share/api/index.ts
+++ src/share/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.BASE_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const authService = {
     async login(initData: string) {
